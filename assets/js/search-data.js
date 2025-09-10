d05e72aeae6dfde700f2aa0595995b99c3507366
alexgrlt <98583667+alexgrlt@users.noreply.github.com>
2025-09-10 09:37:29 +0000

    Deploying to gh-pages from @ alexgrlt/alexgrlt.github.io@c4d64c16ce2ac2127f76da6e4fd3f5fbf745f00d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,8 +9,8 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-",
-          title: "",
+  },{id: "nav-teaching",
+          title: "teaching",
           description: "",
           section: "Navigation",
           handler: () => {
