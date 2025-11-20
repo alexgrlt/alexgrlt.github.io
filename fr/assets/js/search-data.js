e7e1fbdd17dae1214f431eb85589f4dbfa11cc78
alexgrlt <98583667+alexgrlt@users.noreply.github.com>
2025-11-20 14:59:46 +0000

    Deploying to gh-pages from @ alexgrlt/alexgrlt.github.io@8f1ebb3e16c4fa9ccb8d8b728e5dfc85d47ef863 🚀

--- fr/assets/js/search-data.js
+++ fr/assets/js/search-data.js
@@ -28,7 +28,7 @@ ninja.data = [{
           description: "",
           section: "News",},{id: "projects-reading-with-ai-can-technology-foster-reading-acquisition-and-promote-educational-equity",
           title: 'Reading with AI. Can Technology Foster Reading Acquisition and Promote Educational Equity?',
-          description: "Exploring the use of ML-based platforms to improve reading skills in first-grade",
+          description: "Co-authors: [Carlo Barone](https://www.sciencespo.fr/cris/en/directory/barone-carlo/), Marc Gurgand, [Elise Huillery](https://sites.google.com/site/elisehuillery/), Thomas Villemonteix",
           section: "Projects",handler: () => {
               window.location.href = "/projects/en/Lalilo/";
             },},{
