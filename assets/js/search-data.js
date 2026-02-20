f730dd8d893a109af7fb03e1a01598d0ac7a2be4
alexgrlt <98583667+alexgrlt@users.noreply.github.com>
2026-02-20 13:38:40 +0000

    Deploying to gh-pages from @ alexgrlt/alexgrlt.github.io@13e36b0632958ec6fddea60a2dd52c86b2f190d6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -14,7 +14,7 @@ ninja.data = [{
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/assets/teaching/PAPE/PAPE/";
+            window.location.href = "/assets/teaching/PAPE/";
           },
         },{id: "nav-teaching",
           title: "teaching",
